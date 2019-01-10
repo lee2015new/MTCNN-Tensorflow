389e61af10897ca6e7d8c32711ab422c42089e01
liwei63 <liwei63@jd.com>
2019-01-10 18:49:06 +0800

    自己的数据用MTCNN对其之后再用facenet提取特征

--- test/RealtimeIdentification.py
+++ test/RealtimeIdentification.py
@@ -42,17 +42,44 @@ def face2database(picture_path,model_path,database_path,batch_size=90,image_size
             nrof_images = len(paths)
             nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / batch_size))
             emb_array = np.zeros((nrof_images, embedding_size))
+
+            mtcnn_detector = getMtcnnDetector()
+
             for i in range(nrof_batches_per_epoch):
                 start_index = i*batch_size
                 end_index = min((i+1)*batch_size, nrof_images)
                 paths_batch = paths[start_index:end_index]
-                images = facenet.load_data(paths_batch, False, False,image_size)
+                # images = facenet.load_data(paths_batch, False, False,image_size)
+                images = load_data_mtcnn(mtcnn_detector, paths_batch, image_size)
                 feed_dict = { images_placeholder:images, phase_train_placeholder:False }
                 emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
             np.savez(database_path,emb=emb_array,lab=labels, labNm=labels_name)
             print(emb_array)
             print("数据库特征提取完毕！")
             #emb_array里存放的是图片特征，labels为对应的标签
+
+def load_data_mtcnn(mtcnn_detector, image_paths, image_size):
+    nrof_samples = len(image_paths)
+    images = np.zeros((nrof_samples, image_size, image_size, 3))
+    for i in range(nrof_samples):
+        img = misc.imread(image_paths[i], mode="RGB")
+        img_size = np.array(img.shape)[0:2]
+        boxes_c, landmarks = mtcnn_detector.detect(img)
+        bbox = boxes_c[0, :4]
+        # print("#################", image_paths[i], boxes_c, boxes_c.shape[0], bbox)
+
+        corpbbox = [int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])]
+        x1 = np.maximum(int(bbox[0]) - 16, 0)
+        y1 = np.maximum(int(bbox[1]) - 16, 0)
+        x2 = np.minimum(int(bbox[2]) + 16, img_size[1])
+        y2 = np.minimum(int(bbox[3]) + 16, img_size[0])
+        crop_img = img[y1:y2, x1:x2]
+        img2 = misc.imresize(crop_img, (160, 160), interp='bilinear')
+        img2 = np.reshape(img2, (-1, 160, 160, 3))
+        images[i, :, :, :] = img2
+
+    # print(images)
+    return images
 
 
 def ClassifyTrainSVC(database_path,SVCpath):
@@ -98,25 +125,9 @@ def RTrecognization(facenet_model_path, SVCpath, database_path):
             Database = np.load(database_path)
 
             test_mode = "onet"
-            thresh = [0.9, 0.6, 0.7]
-            min_face_size = 24
-            stride = 2
-            slide_window = False
-            shuffle = False
-            # vis = True
-            detectors = [None, None, None]
-            prefix = ['../data/MTCNN_model/PNet_landmark/PNet', '../data/MTCNN_model/RNet_landmark/RNet',
-                      '../data/MTCNN_model/ONet_landmark/ONet']
-            epoch = [18, 14, 16]
-            model_path = ['%s-%s' % (x, y) for x, y in zip(prefix, epoch)]
-            PNet = FcnDetector(P_Net, model_path[0])
-            detectors[0] = PNet
-            RNet = Detector(R_Net, 24, 1, model_path[1])
-            detectors[1] = RNet
-            ONet = Detector(O_Net, 48, 1, model_path[2])
-            detectors[2] = ONet
-            mtcnn_detector = MtcnnDetector(detectors=detectors, min_face_size=min_face_size,
-                                           stride=stride, threshold=thresh, slide_window=slide_window)
+
+            mtcnn_detector = getMtcnnDetector()
+
             video_capture = cv2.VideoCapture(0)
             # video_capture.set(3, 340)
             # video_capture.set(4, 480)
@@ -194,11 +205,34 @@ def RTrecognization(facenet_model_path, SVCpath, database_path):
             cv2.destroyAllWindows()
 
 
+def getMtcnnDetector():
+    thresh = [0.9, 0.6, 0.7]
+    min_face_size = 24
+    stride = 2
+    slide_window = False
+    shuffle = False
+    # vis = True
+    detectors = [None, None, None]
+    prefix = ['../data/MTCNN_model/PNet_landmark/PNet', '../data/MTCNN_model/RNet_landmark/RNet',
+              '../data/MTCNN_model/ONet_landmark/ONet']
+    epoch = [18, 14, 16]
+    model_path = ['%s-%s' % (x, y) for x, y in zip(prefix, epoch)]
+    PNet = FcnDetector(P_Net, model_path[0])
+    detectors[0] = PNet
+    RNet = Detector(R_Net, 24, 1, model_path[1])
+    detectors[1] = RNet
+    ONet = Detector(O_Net, 48, 1, model_path[2])
+    detectors[2] = ONet
+    mtcnn_detector = MtcnnDetector(detectors=detectors, min_face_size=min_face_size,
+                                   stride=stride, threshold=thresh, slide_window=slide_window)
+    return mtcnn_detector
+
 if __name__ == "__main__":
     picture_path = "../data/face_database"
+    # picture_path = "../data/data_back"
     model_path = "../data/face_models/20180408-102900"
     database_path = "../data/database/Database.npz"
     SVCpath = "../data/SVCmodel/SVCmodel.pkl"
-    face2database(picture_path, model_path, database_path)
-    ClassifyTrainSVC(database_path, SVCpath)
-    # RTrecognization(model_path, SVCpath, database_path)
+    # face2database(picture_path, model_path, database_path)
+    # ClassifyTrainSVC(database_path, SVCpath)
+    RTrecognization(model_path, SVCpath, database_path)
